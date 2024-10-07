d298e1fa20f34dfe55ec4d319a63a69cf636a4a5
Xav <bonjour@xaviers.sh>
2024-10-07 12:59:36 -0400

    WIP Tldr command output

--- src/components/TldrOutput.vue
+++ src/components/TldrOutput.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import figlet from 'figlet'
+import standard from 'figlet/importable-fonts/Standard.js'
 
-<template><div /></template>
+const figletText = ref('')
+
+onMounted(() => {
+  figlet.parseFont('Standard', standard)
+  figlet.text(
+    'Xavier Saliniere',
+    {
+      font: 'Standard'
+    },
+    (_, data) => (figletText.value = data ?? '')
+  )
+})
+</script>
+
+<template>
+  <div class="font-mono text-xs leading-none whitespace-break-spaces">
+    {{ figletText }}
+  </div>
+</template>
 
 <style scoped lang="scss"></style>
