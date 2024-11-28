7f72c0644929b8d92e67e558c9709a255a74c550
Xav <bonjour@xaviers.sh>
2024-11-28 11:24:52 -0500

    Pro title update

--- src/content/en/intro-output.ts
+++ src/content/en/intro-output.ts
@@ -1,7 +1,7 @@
 export default {
   welcome: 'Welcome to my terminal-inspired Portfolio!',
   iam: "I'm",
-  workTitle: ', a senior Web app developer.',
+  workTitle: ', senior Web app developer and junior Blockchain developer.',
   tabBrowsing: 'This porfolio app can be browsed by clicking on the tabs above.',
   cmdBrowsing: 'Or, for the command line experience, get started with',
   clickable: '(<= clickable)'
--- src/content/fr/intro-output.ts
+++ src/content/fr/intro-output.ts
@@ -1,7 +1,7 @@
 export default {
   welcome: 'Bienvenue sur mon portfolio au look de terminal!',
   iam: 'Je suis',
-  workTitle: ", un développeur senior d'applications Web.",
+  workTitle: ", développeur senior d'applications Web et développeur junior Blockchain.",
   tabBrowsing:
     'Cette application porfolio peut être explorée en cliquant sur les onglets ci-dessus.',
   cmdBrowsing: 'Ou, pour une expérience terminal, commencez par',
